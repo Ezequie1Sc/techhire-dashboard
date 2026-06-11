f801c241dc8b665410352626ff9b53a56a55691a
Ezequiel Salazar <ezequielsc017@gmail.com>
2026-06-10 21:27:36 -0600

    feat(routes): agregar manejo de rutas no encontradas con página 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { JobsComponent } from './pages/jobs/jobs.component';
 import { JobDetailComponent } from './pages/job-detail/job-detail.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'jobs', component: JobsComponent },
   { path: 'jobs/:slug', component: JobDetailComponent },
   { path: 'favorites', component: FavoritesComponent },
-  { path: '**', redirectTo: '' }
+
+  // Página 404
+  { path: '**', component: NotFoundComponent }
 ];
